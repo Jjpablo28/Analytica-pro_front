6d092af0c23463da058a1e0c1ee79b0b7aabb5e2
Jjpablo28 <jpr.rod06@gmail.com>
2025-11-18 07:48:45 -0500

    new react

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Aurora from "./components/Aurora/Aurora.jsx";
+
+function App() {
+
+  return (
+      <Aurora
+          colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+          blend={0.5}
+          amplitude={1.0}
+          speed={0.5}
+      />
+  )
+}
+
+export default App
